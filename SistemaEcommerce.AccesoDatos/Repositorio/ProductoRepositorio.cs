b8af3d640b886d8ce933ea6a110dc6f62c9924ee
Daniel Flores <vflorestrejo@gmail.com>
2024-04-17 17:25:56 -0600

    Add Producto Frontend

---
+++ SistemaEcommerce.AccesoDatos/Repositorio/ProductoRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using SistemaEcommerce.AccesoDatos.Data;
+using SistemaEcommerce.AccesoDatos.Repositorio.IRepositorio;
+using SistemaEcommerce.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEcommerce.AccesoDatos.Repositorio
+{
+    public class ProductoRepositorio : Repositorio<Producto>, IProductoRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductoRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Actualizar(Producto producto)
+        {
+            var productoDB = _db.Productos.FirstOrDefault(b => b.Id == producto.Id);
+            if (productoDB != null)
+            {
+                if (producto.ImagenUrl != null)
+                {
+                    productoDB.ImagenUrl = producto.ImagenUrl;
+                }
+
+                productoDB.NumeroSerie = producto.NumeroSerie;
+                productoDB.Descripcion = producto.Descripcion;
+                productoDB.Precio = producto.Precio;
+                productoDB.Costo = productoDB.Costo;
+                productoDB.CategoriaId = productoDB.CategoriaId;
+                productoDB.MarcaId = productoDB.MarcaId;
+                productoDB.PadreId = productoDB.PadreId;
+                productoDB.Estado = producto.Estado;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
